d3eda2a74edc2ade9adada2a271ddb2f731384f8
Oumaima TORAIF <69909159+oumaimatoraif@users.noreply.github.com>
2022-04-08 14:59:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ header("location:products.php");
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="google-site-verification" content="google-site-verification: google573bf03d99e821fe.html">
     <script
       src="https://kit.fontawesome.com/64d58efce2.js"
       crossorigin="anonymous"
